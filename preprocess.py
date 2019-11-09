8746aa96e197e39b4d06aead8dcf9ebb44252fe5
StarMa_Home <anxiang1836@126.com>
2019-11-09 13:35:17 +0800

    Update method of preprocess

--- preprocess.py
+++ preprocess.py
@@ -26,34 +26,36 @@ class PrepareData(object):
         self.stop_word_path = stop_word_path
         self.output_w2v_path = ""
 
-        self._data = pd.read_csv(self.input_csv_path, sep="###_###", header=None, encoding='utf-8')
+        self._data = pd.read_csv(self.input_csv_path, sep="###__###", header=None, encoding='utf-8')
         self._data.columns = ['ID', 'Age', 'Gender', 'Education', 'Query_list']
 
-    @property
-    def stop_words(self):
         file = open(self.stop_word_path, encoding='utf-8')
         stop_words_list = file.readlines()
         file.close()
-        return [word.strip() for word in stop_words_list]
+        self.stopwords = [word.strip() for word in stop_words_list]
 
-    # ----------重写数据-----------------
+
+# ----------重写数据-----------------
     def read_and_rewrite(self):
-
         data = self._data
         # -- 每个人的queryList统计量计算 --
 
         # 统计每个人的query数量
         data['Query_count'] = data['Query_list'].map(lambda x: len(x.split('\t')))
         # 统计每个人的包含空格的query个数
-        data['Query_has_space'] = data['Query_list'].map(lambda x: PrepareData.__count_query_has_space(x.split('\t')))
+        data['Query_has_space'] = data['Query_list'].map(self.__count_query_has_space)
+        print("数据统计量计算完毕！")
 
         # -- 清洗数据 --
-        data['Query_list'] = data['Query_list'].map(PrepareData.__clean_query)
+        data['Query_list'] = data['Query_list'].map(self.__clean_query)
+        print("数据清洗完毕！！")
 
         # -- 补全缺失值 --
-        tfv = TfidfVectorizer(tokenizer=PrepareData.__Tokenizer(), min_df=3, max_df=0.95, sublinear_tf=True)
+        tfv = TfidfVectorizer(tokenizer=self.__Tokenizer(stopwords=self.stopwords), min_df=3, max_df=0.95,
+                              sublinear_tf=True)
         X_tfidf = tfv.fit_transform(data['Query_list'])
         pickle.dump(X_tfidf, open(self.output_tfidf_path, 'wb'))  # 持久化
+        print("tfidf-vector持久化完毕！")
 
         for label in ['Age', 'Gender', 'Education']:
             train_data = data.loc[data[label] != 0, label]
@@ -68,10 +70,12 @@ class PrepareData(object):
             data[label][to_befilled_index] = clf.predict(X_tfidf[to_befilled_index])
 
         self._data = data
+        print('数据补全完毕！')
 
         # 持久化数据表
         data.to_csv(self.output_csv_path, index=None, encoding='utf-8')
         return
+
 
     # --------准备W2v训练数据---------------
     # def prepare_for_w2v(self):
@@ -87,33 +91,37 @@ class PrepareData(object):
             if re.match(r'https?://', query.strip()) is not None:
                 continue
             # 删除掉所有数字
-            query = re.sub(r'\d', '', query)
+            query = re.sub(r'\d', '', query.strip())
             if len(query) != 0:
                 new_list += query
             # 替换所有的公式
             # TODO 找到公式替换规则（数据发现再去看）
         return '\t'.join(new_list)
 
+
     # ----------统计空格率静态方法-----------
     @staticmethod
     def __count_query_has_space(query_list):
         count = 0
+        query_list = query_list.split('\t')
         for query in query_list:
             if re.match(r'\s', query.strip()) is not None:
                 count += 1
         # 返回一个比例，即有space的query占全部总query数的比例
         return count / len(query_list)
 
+
     class __Tokenizer:
-        def __init__(self):
+        def __init__(self, stopwords):
             self.n = 0
+            self.stopwords = stopwords
 
         def __call__(self, line):
             tokens = []
             for query in line.split('\t'):
                 word_list = jieba.lcut(query)
                 # 去停用词
-                word_list = [word for word in word_list if word not in PrepareData.stop_words]
+                word_list = [word for word in word_list if word not in self.stopwords]
 
                 for n_gram in [1, 2]:
                     for i in range(len(word_list) - n_gram + 1):
@@ -122,10 +130,10 @@ class PrepareData(object):
 
 
 if __name__ == '__main__':
-    params = {'input_csv_path': '',
-              'output_csv_path': '',
-              'output_tfidf_path': '',
-              'stop_word_path': ''}
+    params = {'input_csv_path': './data/train.csv',
+              'output_csv_path': './data/new_train_20191110.csv',
+              'output_tfidf_path': './data/tfidf/vector_20191110.pkl',
+              'stop_word_path': './data/stopwords_hit.txt'}
 
     preprocess = PrepareData(**params)
     preprocess.read_and_rewrite()
